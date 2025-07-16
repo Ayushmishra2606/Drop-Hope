a8728e4982008fb34c46b8e31d5e5354940db9a7
Ayush <imayush2702@gmail.com>
2025-07-17 01:39:28 +0530

    Signup on

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,14 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 // import Userdashboard from "./pages/User-dashboard";
 import NGOdashboard from "./pages/NGO-dashboard";
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/Login'
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <> 
-      
+      <Signup/>
     </>
   );
 }
