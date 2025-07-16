76381152551c10f796b58533385c3ae291cd53d9
Ayush <imayush2702@gmail.com>
2025-07-16 16:11:52 +0530

    tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    
-      
-
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Drop-hope</h1>
-      <h1>chl gyi</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <h1>Kautuk</h1>
-
+    <>
+    <h1 className='text-3xl text-amber-200'>Ayush</h1>
     </>
   )
 }
