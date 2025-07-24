796992aca4f7252f9a49af8320ce7fc636d60ade
Kautuk <kautukpandey2004@gmail.com>
2025-07-24 22:59:01 +0530

    Remove Firebase credentials from repo

--- back-end/index.js
+++ back-end/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 connectDB();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://drop-hope-frontend.vercel.app/",
   credentials: true
 }));
 app.use(express.json());
