02842acf809535af10de62dca0613a7e6766e358
Kautuk <kautukpandey2004@gmail.com>
2025-07-24 22:18:07 +0530

    Remove Firebase credentials from repo

--- back-end/config/multer.config.js
+++ back-end/config/multer.config.js
@@ -2,18 +2,13 @@ import multer from 'multer';
 import firebaseStorage from 'multer-firebase-storage';
 import dotenv from 'dotenv';
 dotenv.config();
-// Read service account credentials from FIREBASE_CONFIG env variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-
-// Extract credentials
-const { client_email, private_key, project_id } = serviceAccount;
 
 // Set up multer-firebase-storage
 const storage = firebaseStorage({
-  bucketName: 'ayush-901e4.appspot.com', // ✅ corrected bucket name
+  bucketName: 'ayush-901e4.appspot.com',
   credentials: {
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // handle newline chars
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     projectId: process.env.FIREBASE_PROJECT_ID,
   },
   unique: true,
